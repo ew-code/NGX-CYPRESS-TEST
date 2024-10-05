db281d405704c08a37699e87df1cffbd50a80a46
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2024-10-05 12:56:00 +0200

    test Datepicker refactor

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -145,18 +145,31 @@ describe('First test suite', () => {
     })
 
     it.only('25.Datepicker', () => {
+        function selectDayFromCurrent(day) {
+            let date = new Date()
+            date.setDate(date.getDate() + day)
+            let futureDay = date.getDate()
+            let futureMonth = date.toLocaleDateString('en-US', { month: 'short' })
+            let futureYear = date.getFullYear()
+            let dateToAssert = `${futureMonth} ${futureDay}, ${futureYear}`
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(dateAttribute => {
+                if (!dateAttribute.includes(futureMonth) || !dateAttribute.includes(futureYear)) {
+                    cy.get('[data-name="chevron-right"]').click()
+                    selectDayFromCurrent(day)
+                } else {
+                    cy.get('.day-cell').not('.bounding-month').contains(futureDay).click()
+                }
+            })
+            return dateToAssert
+        }
+
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Datepicker').click()
-
-        let date = new Date()
-        date.setDate(date.getDate() + 5)
-        let futureDate = date.getDate()
-        let dateToAssert = `Oct ${futureDate }, 2024`
-
         cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
             cy.wrap(input).click()
-            cy.get('.day-cell').not('.bounding-month').contains(futureDate).click()
+            const dateToAssert = selectDayFromCurrent(5)
+            selectDayFromCurrent()
             cy.wrap(input).invoke('prop', 'value').should('contain', dateToAssert)
             cy.wrap(input).should('have.value', dateToAssert)
         })
