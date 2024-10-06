3b707101fee6ccc8ea02be38571b11ae0d2fc56b
Ewelina Wesołowska <64744933+ew-code@users.noreply.github.com>
2024-10-06 14:22:06 +0200

    chore: new Pages and second Test added

--- cypress/e2e/testWithPageObject.spec.js
+++ cypress/e2e/testWithPageObject.spec.js
@@ -1,4 +1,7 @@
+import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage"
 import { navigateTo } from "../support/page_objects/navigationPage"
+import { onSmartTablePage } from "../support/page_objects/smartTablePage"
+import { onDatepickerPage } from "./datapickerPage"
 
 describe('Test with Page Objects', () => {
 
@@ -12,5 +15,17 @@ describe('Test with Page Objects', () => {
         navigateTo.toastrPage()
         navigateTo.smartTablePage()
         navigateTo.tooltipPage()
+    })
+
+    it.only('should submit Inline and Basic form and select tomorow date in the calendar', () => {
+        navigateTo.formLayoutsPage()
+        onFormLayoutsPage.submitInlineFormWithNameAndEmail('Ewelina', 'test@test.com')
+        onFormLayoutsPage.submitBasicFormWithNameAndEmail('test@test.com', 'password')
+        navigateTo.datepickerPage()
+        onDatepickerPage.selectCommonDatepickerDateFromToday(30)
+        onDatepickerPage.selectDatepickerWithRangeFromToday(7, 14)
+        onSmartTablePage.addNewRecordWithFirstAndLastName('Ewelina', 'Test')
+        onSmartTablePage.updateAgeByFirstName('Ewelina', '25')
+        onSmartTablePage.deleteRowByIndex(1)
     })
 })
